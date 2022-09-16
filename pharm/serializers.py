b2173c697786d6126f4932764c59441b4f4aed62
Otman LAHRECHE <laotman.47@gmail.com>
2022-09-16 10:27:44 +0100

    finaling the serializers

--- pharm/serializers.py
+++ pharm/serializers.py
@@ -48,6 +48,37 @@ class FournisseurSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'address', 'email_adress', 'phone_nbr']
 
 
+class CommandeItemsSerializer(serializers.ModelSerializer):
+    medicament = MedicamentSerialize()
+    class Meta:
+        model = Sortie_items
+        fields = ['id', 'commande_qte', 'medicament']
+
+
+class BonCommandeSerializer(serializers.ModelSerializer):
+    fournisseur = FournisseurSerializer()
+    commande_items = CommandeItemsSerializer(many=True)
+    class Meta:
+        model = Bon_sortie
+        fields = ['id', 'fournisseur', 'bon_commande_nbr', 'date', 'commande_items']
+
+
+class ArivageItemsSerializer(serializers.ModelSerializer):
+    medicament = MedicamentSerialize()
+
+    class Meta:
+        model = Arivage_items
+        fields = ['id', 'medicament', 'date_expired', 'qnt']
+
+
+class ArivageSerializer(serializers.ModelSerializer):
+    arivage_items = ArivageItemsSerializer(many=True)
+
+    class Meta:
+        model = Arivage
+        fields = ['id', 'date', 'source_detail', 'arivage_items']
+
+
 
 
 
