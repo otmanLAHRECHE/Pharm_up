5b068e4b7093087617dadcc64643c627eefdd210
Otman LAHRECHE <laotman.47@gmail.com>
2022-10-05 14:35:55 +0100

    updates

--- frontend/src/components/pharmacy/Stock.js
+++ frontend/src/components/pharmacy/Stock.js
@@ -46,132 +46,7 @@ const columns = [
     { field: 'stock_qte', headerName: 'QNT', width: 150 },
   ];
 
-  const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 },
-    {
-      label: 'The Lord of the Rings: The Return of the King',
-      year: 2003,
-    },
-    { label: 'The Good, the Bad and the Ugly', year: 1966 },
-    { label: 'Fight Club', year: 1999 },
-    {
-      label: 'The Lord of the Rings: The Fellowship of the Ring',
-      year: 2001,
-    },
-    {
-      label: 'Star Wars: Episode V - The Empire Strikes Back',
-      year: 1980,
-    },
-    { label: 'Forrest Gump', year: 1994 },
-    { label: 'Inception', year: 2010 },
-    {
-      label: 'The Lord of the Rings: The Two Towers',
-      year: 2002,
-    },
-    { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { label: 'Goodfellas', year: 1990 },
-    { label: 'The Matrix', year: 1999 },
-    { label: 'Seven Samurai', year: 1954 },
-    {
-      label: 'Star Wars: Episode IV - A New Hope',
-      year: 1977,
-    },
-    { label: 'City of God', year: 2002 },
-    { label: 'Se7en', year: 1995 },
-    { label: 'The Silence of the Lambs', year: 1991 },
-    { label: "It's a Wonderful Life", year: 1946 },
-    { label: 'Life Is Beautiful', year: 1997 },
-    { label: 'The Usual Suspects', year: 1995 },
-    { label: 'Léon: The Professional', year: 1994 },
-    { label: 'Spirited Away', year: 2001 },
-    { label: 'Saving Private Ryan', year: 1998 },
-    { label: 'Once Upon a Time in the West', year: 1968 },
-    { label: 'American History X', year: 1998 },
-    { label: 'Interstellar', year: 2014 },
-    { label: 'Casablanca', year: 1942 },
-    { label: 'City Lights', year: 1931 },
-    { label: 'Psycho', year: 1960 },
-    { label: 'The Green Mile', year: 1999 },
-    { label: 'The Intouchables', year: 2011 },
-    { label: 'Modern Times', year: 1936 },
-    { label: 'Raiders of the Lost Ark', year: 1981 },
-    { label: 'Rear Window', year: 1954 },
-    { label: 'The Pianist', year: 2002 },
-    { label: 'The Departed', year: 2006 },
-    { label: 'Terminator 2: Judgment Day', year: 1991 },
-    { label: 'Back to the Future', year: 1985 },
-    { label: 'Whiplash', year: 2014 },
-    { label: 'Gladiator', year: 2000 },
-    { label: 'Memento', year: 2000 },
-    { label: 'The Prestige', year: 2006 },
-    { label: 'The Lion King', year: 1994 },
-    { label: 'Apocalypse Now', year: 1979 },
-    { label: 'Alien', year: 1979 },
-    { label: 'Sunset Boulevard', year: 1950 },
-    {
-      label: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-      year: 1964,
-    },
-    { label: 'The Great Dictator', year: 1940 },
-    { label: 'Cinema Paradiso', year: 1988 },
-    { label: 'The Lives of Others', year: 2006 },
-    { label: 'Grave of the Fireflies', year: 1988 },
-    { label: 'Paths of Glory', year: 1957 },
-    { label: 'Django Unchained', year: 2012 },
-    { label: 'The Shining', year: 1980 },
-    { label: 'WALL·E', year: 2008 },
-    { label: 'American Beauty', year: 1999 },
-    { label: 'The Dark Knight Rises', year: 2012 },
-    { label: 'Princess Mononoke', year: 1997 },
-    { label: 'Aliens', year: 1986 },
-    { label: 'Oldboy', year: 2003 },
-    { label: 'Once Upon a Time in America', year: 1984 },
-    { label: 'Witness for the Prosecution', year: 1957 },
-    { label: 'Das Boot', year: 1981 },
-    { label: 'Citizen Kane', year: 1941 },
-    { label: 'North by Northwest', year: 1959 },
-    { label: 'Vertigo', year: 1958 },
-    {
-      label: 'Star Wars: Episode VI - Return of the Jedi',
-      year: 1983,
-    },
-    { label: 'Reservoir Dogs', year: 1992 },
-    { label: 'Braveheart', year: 1995 },
-    { label: 'M', year: 1931 },
-    { label: 'Requiem for a Dream', year: 2000 },
-    { label: 'Amélie', year: 2001 },
-    { label: 'A Clockwork Orange', year: 1971 },
-    { label: 'Like Stars on Earth', year: 2007 },
-    { label: 'Taxi Driver', year: 1976 },
-    { label: 'Lawrence of Arabia', year: 1962 },
-    { label: 'Double Indemnity', year: 1944 },
-    {
-      label: 'Eternal Sunshine of the Spotless Mind',
-      year: 2004,
-    },
-    { label: 'Amadeus', year: 1984 },
-    { label: 'To Kill a Mockingbird', year: 1962 },
-    { label: 'Toy Story 3', year: 2010 },
-    { label: 'Logan', year: 2017 },
-    { label: 'Full Metal Jacket', year: 1987 },
-    { label: 'Dangal', year: 2016 },
-    { label: 'The Sting', year: 1973 },
-    { label: '2001: A Space Odyssey', year: 1968 },
-    { label: "Singin' in the Rain", year: 1952 },
-    { label: 'Toy Story', year: 1995 },
-    { label: 'Bicycle Thieves', year: 1948 },
-    { label: 'The Kid', year: 1921 },
-    { label: 'Inglourious Basterds', year: 2009 },
-    { label: 'Snatch', year: 2000 },
-    { label: '3 Idiots', year: 2009 },
-    { label: 'Monty Python and the Holy Grail', year: 1975 },
-  ];
+  
 
 export default function Stock(){
 
@@ -193,8 +68,8 @@ export default function Stock(){
     const [responseSuccesSignal, setResponseSuccesSignal] = React.useState(false);
     const [responseErrorSignal, setResponseErrorSignal] = React.useState(false);
 
-    const [checkNames, setCheckNames] = React.useState(false);
-    const [checkArivage, setCheckArivage] = React.useState(false);
+    const [allNames, setAllNames] = React.useState([]);
+    const [allArivage, setAllArivage] = React.useState([]);
 
     const [data, setData] = React.useState([]);
     const [namesData, setNamesData] = React.useState([]);
@@ -228,9 +103,7 @@ export default function Stock(){
       }
 
       const addStockOpen = async () =>{
-        
-        setServiceName("");
-        setServiceService("");
+      
 
         setMedicName(null);
         setArivage(null);
@@ -244,7 +117,7 @@ export default function Stock(){
 
         const token = localStorage.getItem("auth_token");
 
-        setNamesData(await getAllMedicNames(toke));
+        setNamesData(await getAllMedicNames(token));
       }
 
       const addStockSave = async () =>{
@@ -262,24 +135,13 @@ export default function Stock(){
 
       React.useEffect(() =>{
         console.log(namesData);
-
-
         try{
-          if (rowData == "no data"){
+          if (namesData == "no data"){
             setResponseErrorSignal(true);
-          } else if(rowData != "") {
-    
-          setOpenUpdate(true);
-          console.log(rowData.id)
-    
-          setServiceName(rowData.name);
-          setServiceService(rowData.service);
-  
-          setServiceNameError([false, ""]);
-          setServiceServiceError([false, ""]);
-  
+          } else if(namesData != "") {
+            setAllNames(namesData);
+            setOpen(true);
           }
-
         }catch(e){
           console.log(e);
         }
@@ -349,13 +211,15 @@ export default function Stock(){
                                             value={medicName}
                                             onChange={(event, newVlue) =>{
                                                 setMedicName(newVlue);
+                                                console.log(newValue);
                                                 
                                             }}
                                             id="combo-box-demo"
-                                            options={top100Films}
+                                            options={allNames}
                                             sx={{ width: 300 }}
                                             renderInput={(params) => <TextField {...params} error={medicNameError[0]}
-                                            helperText={medicNameError[1]} fullWidth variant="standard" label="Médicaments" />}
+                                            helperText={medicNameError[1]} fullWidth variant="standard" label="Médicaments" 
+                                            required/>}
                                         />
 
                                         </Grid>
@@ -368,10 +232,11 @@ export default function Stock(){
                                                         setArivage(newVlue);
                                                         
                                                     }}
-                                                    options={top100Films}
+                                                    options={allArivage}
                                                     sx={{ width: 300 }}
                                                     renderInput={(params) => <TextField {...params} error={arivageError[0]}
-                                                    helperText={arivageError[1]} fullWidth variant="standard" label="Médicaments" />}
+                                                    helperText={arivageError[1]} fullWidth variant="standard" label="Arrivage" 
+                                                    required/>}
                                                 />  
                                         
                                         </Grid>
@@ -387,12 +252,13 @@ export default function Stock(){
                                             <Grid item xs={6}>
                                             <LocalizationProvider dateAdapter={AdapterDayjs}>
                                                 <DesktopDatePicker
-                                                        label="Date d arivage"
+                                                        label="Date d arrivage"
                                                         inputFormat="MM/DD/YYYY"
                                                         value={dateArived}
                                                         onChange={handleChangeDateArived}
                                                         renderInput={(params) => <TextField {...params} error={dateArivedError[0]}
-                                                        helperText={dateArivedError[1]} />}
+                                                        helperText={dateArivedError[1]} 
+                                                        required/>}
                                                 />
 
                                             </LocalizationProvider>
@@ -408,7 +274,8 @@ export default function Stock(){
                                                         value={dateExpired}
                                                         onChange={handleChangeDateExpired}
                                                         renderInput={(params) => <TextField {...params} error={dateExpiredError[0]}
-                                                        helperText={dateExpiredError[1]} />}
+                                                        helperText={dateExpiredError[1]} 
+                                                        required />}
                                                 />
                                             </LocalizationProvider>
                                             
