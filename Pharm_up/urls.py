24259dcce9abcd8c009ad4d149fe1bed8e772404
Otman LAHRECHE <laotman.47@gmail.com>
2022-10-19 14:20:21 +0100

    url configuration for the frontend roots

--- Pharm_up/urls.py
+++ Pharm_up/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
     path('accounts/', include('accounts.urls')),
-    path('pharm/', include('pharm.urls'))
+    path('pharm/', include('pharm.urls')),
+    re_path(r'^(?:.*)/?$', include('frontend.urls')),
 ]
